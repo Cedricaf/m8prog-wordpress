4ce8277ecdb91ad6c79563c6128cc5f34ce72c9d
Cedricaf <34737@ma-web.nl>
2024-05-30 17:20:19 +0200

    modified

--- themes/cedrictheme/footer.php
+++ themes/cedrictheme/footer.php
@@ -8,10 +8,16 @@
  */
 ?>
 
-<footer>
-    <p>dit is de footer van de pagina.</p>
-    <?php wp_footer(); ?>
+<footer class="footer mt-auto py-3 bg-body-tertiary">
+    <div class="container">
+        <span class="text-body-secondary">Place footer content here.</span>
+    </div>
 </footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous">
+</script>
 
 </body>
 </html>
