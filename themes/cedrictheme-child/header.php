07a0dc0d6696485ee8e63f7d1c5bf5e64c85bf9c
Cedricaf <34737@ma-web.nl>
2024-06-03 15:43:22 +0200

    modified

---
+++ themes/cedrictheme-child/header.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * The header.
+ *
+ * This is the template that displays all of the <head> section and everything up until main.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since 1.0.0
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title><?php bloginfo('name'); ?></title>
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+    <?php wp_body_open(); ?>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <?php
+                wp_nav_menu(
+                    [
+                        'menu'            => 'header',
+                        'link_before'     => '',
+                        'link_after'      => '',
+                        'menu_class'      => 'navbar-nav me-auto mb-2 mb-lg-0',
+                        'container'       => 'ul',
+                        'container_class' => '',
+                        'container_id'    => '',
+                        'add_li_class'    => 'nav-item',
+                        'add_a_class'     => 'nav-link',
+                    ]
+                );
+                ?>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+
+
+<?php
+
--- themes/cedrictheme-child/single.php
+++ themes/cedrictheme-child/single.php
@@ -6,8 +6,8 @@
     <h1 class="mt-5"><?php the_title(); ?></h1>
     <div class="col-lg-8 px-0">
         <?php the_excerpt(); ?>
-        <<?php the_date(); ?>
-        <<?php the_author_meta('description'); ?>
+        <?php the_date(); ?>
+        <?php the_author_meta('description'); ?>
     </div>
 </main>
 
